3b0b91ac747cd08d293ca44d5f91645cc95a83ae
Jacob Rosenthal <jakerosenthal@gmail.com>
2017-11-26 22:18:10 -0700

    carthage framework scheme

---
+++ Project/Pods/SimpleExif/SimpleExif.h
@@ -0,0 +1,18 @@
+//
+//  SimpleExif.h
+//  SimpleExif
+//
+//  Created by Jacob Rosenthal on 11/26/17.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SimpleExif.
+FOUNDATION_EXPORT double SimpleExifVersionNumber;
+
+//! Project version string for SimpleExif.
+FOUNDATION_EXPORT const unsigned char SimpleExifVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleExif/PublicHeader.h>
+
+#import <SimpleExif/ExifContainer.h>
